8b9cc224a463085a0243be95218d3c5232960691
denis <denis.rybkin.94@mail.ru>
2022-06-19 22:41:58 +0300

    remake git ignore

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ export class App {
     private initMiddlewares(): void {
         this._app.use(express.json());
         this._app.use(cors());
-        this._app.use(function(req, res, next) {
+        this._app.use((req, res, next) =>  {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
